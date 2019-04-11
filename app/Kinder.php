38fa0af5c4c003ba8a227b61291919f8f47cf481
tansenkhan1990 <tansenkhan1990@gmail.com>
2019-04-11 12:37:42 +0200

    add Kinder model

---
+++ app/Kinder.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Kinder extends Model
+{
+    //
+    protected $table='kinder';
+    protected $connection = 'mysql2';
+}
