f28ea13d58cbd433c2c2133fa76765fef30d42f4
tansenkhan1990 <tansenkhan1990@gmail.com>
2019-04-18 09:51:52 +0200

    fixing body function button completely

--- resources/views/personalDetail.blade.php
+++ resources/views/personalDetail.blade.php
@@ -24,7 +24,7 @@
                 <li><a data-toggle="tab" href="#status">status</a></li>
                 <li><a data-toggle="tab" href="#finance">Guest Visit</a></li>
                 <li><a data-toggle="tab" href="#care">Child care</a></li>
-                <li><a data-toggle="tab" href="#body">Body/Function</a></li>
+                <li><a data-toggle="tab" href="#bodyf">Body/Function</a></li>
                 <li><a data-toggle="tab" href="#hactivities">Him Activities</a></li>
                 <li><a data-toggle="tab" href="#profile">Profile</a></li>
                 <li><a data-toggle="tab" href="#profile">Reports</a></li>
@@ -301,6 +301,22 @@
                 </div>
 
 
+            <!--body function start-->
+            @if($bodyFunction!='none')
+                <div id="bodyf" class="tab-pane fade">
+                    <h3>Body function</h3>
+                    <!-- Standard button -->
+                    <div class="panel panel-default">
+                        @foreach($bodyFunction as $bodyf)
+                            <div class="panel-body"><h5>Function:</h5> {{$body2[$bodyf->function]}}</div>
+                            <div class="panel-body"><h5>Stay from</h5> {{$bodyf->von}}</div>
+                            <div class="panel-body"><h5>stay till</h5> {{$bodyf->bis}}</div>
+                        @endforeach
+                    </div>
+                </div>
+        @endif
+        <!--body function end-->
+
                 <div id="paddress" class="tab-pane fade">
                     <h3>Private Address</h3>
                     <div class="panel panel-default">
@@ -456,22 +472,4 @@
 
             </div>
 
-            <!--body function start-->
-
-            <div id="body" class="tab-pane fade">
-                <h3>Body function</h3>
-                <!-- Standard button -->
-
-                <div class="panel panel-default">
-                    @if($bodyFunction!='none')
-                        @foreach($bodyFunction as $bodyf)
-                            <div class="panel-body"><h5>Function:</h5> {{$body2[$bodyf->function]}}</div>
-                            <div class="panel-body"><h5>Stay from</h5> {{$bodyf->von}}</div>
-                            <div class="panel-body"><h5>stay till</h5> {{$bodyf->bis}}</div>
-                        @endforeach
-                    @endif
-                </div>
-            </div>
-
-            <!--body function end-->
 @endsection
